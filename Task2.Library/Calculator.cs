03efc5d925f06fac7b0466002856ed345fda99a8
helenakolodko <helena.kolodko@gmail.com>
2015-07-29 11:04:09 +0300

    Done Task2.

---
+++ Task2.Library/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Library
+{
+    public class Calculator
+    {
+        public static IEnumerable<int> GetFibonacciNumbers(int count)
+        {
+            int prev = 0;
+            int result = 1;
+            yield return result;
+            for (int i = 0; i < count - 1; i++)
+            {
+                int temp = prev;
+                prev = result;
+                result = temp + result;
+                yield return result;
+            }
+        }
+    }
+}
